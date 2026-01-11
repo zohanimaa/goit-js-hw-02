299189d996c71a02bbd584d6ae521c4290a8eefa
Захар <zohanimaa@gmail.com>
2026-01-11 17:08:43 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     if (totalPrice > customerCredits) {
         return "Insufficient funds!";
     } else {
-        return 'You ordered ${quantity} droids worth ${totalPrice} credits!';
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
